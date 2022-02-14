f36cb94d877da71b6095accde350faf7e2a371b6
sophie <sophie1620@gmail.com>
2022-02-14 18:09:48 -0500

    added clothing array and if/else statements

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 //register with Visual Crossing for apiKey
 //Create namespace object called weatherApp
 //Create ARRAY to different Objects representing temperature blocks (with clothing key value properties inside)
@@ -13,30 +11,80 @@
 //Append Array result from if/else statement to page 
 
 
-// const weatherApp = {}
+// where are our object/arrays begin for clothing options
+const runningAttire = [
+     {
+     title: 'extremeCold',
+     clothing: [
+          'hat', 
+          'faceCover', 
+          'neckCover',
+          'gloves', 
+          'mittens', 
+          'longSleeves',
+          'vest',
+          'jacket',
+          'leggings', 
+          'pants', 
+          'socks', 
+          'runningShoes'
+          ]
+     },
 
-// weatherApp.apiURL
-// weatherApp.apiKey = '89Q5FZNA5A9RSN7FCW3A6F2NZ';
+     {
+     title: 'cold', 
+          clothing: [
+          'headband', 
+          'gloves', 
+          'longSleeve',
+          'jacket',
+          'leggings', 
+          'socks', 
+          'runningShoes'
+          ]
+     },
 
+     {
+     title: 'temperate',
+     clothing: [
+          'longSleeve',
+          'vest',
+          'leggings', 
+          'socks', 
+          'runningShoes'
+          ]
+     }, 
 
-// weatherApp.button = document.querySelector('button');
+     {
+     title: 'warm',
+     clothing: [
+          'shortSleeve',
+          'capris', 
+          'socks', 
+          'runningShoes'
+          ]
+     }, 
 
-// weatherApp.button.addEventListener('click', function () {
-//      weatherApp.userLocation = document.querySelector('input').value;
-//      console.log(weatherApp.userLocation);
+     {
+     title: 'hot',
+     clothing: [
+          'tanktop',
+          'shorts', 
+          'socks', 
+          'runningShoes'
+          ]
+     }, 
 
-//      fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${weatherApp.userLocation}?unitGroup=metric&key=${weatherApp.apiKey}`, {
-//           method: "GET",
-//           headers: {
-//           }
-//      })
-//           .then((response) => {
-//                return response.json();
-//           })
-//           .then((jsonResponse) => {
-//                console.log(jsonResponse);
-//           })
-// })
+     {
+     title: 'exteremeHeat',
+     clothing: [
+          'tanktop',
+          'shorts', 
+          'socks', 
+          'runningShoes'
+          ]
+     }
+]
 
 const weatherApp = {}
 
@@ -51,16 +99,21 @@ weatherApp.userInput = document.querySelector('input');
 
 weatherApp.init = function(){
      //add in event listener here- store event listener inside a function weatherapp.eventlistener()
+     console.log(`initialization`);
+     eventListener();
 }
 
-//create event listener to pull user input value 
-//pass user input value into the getWeather function to target user input location
-weatherApp.button.addEventListener('click',function (){
-     weatherApp.userLocation = weatherApp.userInput.value;
-     console.log(weatherApp.userLocation);
-     weatherApp.getWeather(weatherApp.userLocation);
-     weatherApp.userInput.value = "";
-})
+const eventListener = function() {
+     //create event listener to pull user input value 
+     //pass user input value into the getWeather function to target user input location
+     weatherApp.button.addEventListener('click',function (){
+          weatherApp.userLocation = weatherApp.userInput.value;
+          console.log(weatherApp.userLocation);
+          weatherApp.getWeather(weatherApp.userLocation);
+          weatherApp.userInput.value = "";
+     })
+}
+
 
 //pass user input location in as parameter
 //build out new URL to make api call    
@@ -74,14 +127,50 @@ weatherApp.getWeather = function(parameter){
      })
      //api call
      fetch(url)
-          .then((response) => {
-               return response.json();
-          })
-          //parse into json
-          .then((jsonResponse) => {
-               console.log(jsonResponse);
-          })
+     .then((response) => {
+          return response.json();
+     })
+     //parse into json
+     .then((jsonResponse) => {
+          console.log(jsonResponse);
+          weatherApp.displayWeather(jsonResponse);
+     })
 }
+
+// we need to have weather be retrieved
+weatherApp.displayWeather = function(weatherData) {
+     const feelsLike = weatherData.currentConditions.feelslike;
+     weatherApp.displayClothing(feelsLike);
+}
+
+// we need to focus on the .currentConditions.feelslike 
+// we need to identify the current temperater and then match it to the right object-array in the runningAttire array
+weatherApp.displayClothing = function(temperature) {
+     if(temperature < -10) {
+          // console.log(`WAH! COLD! BRR!`);
+          console.log(runningAttire[0].clothing);
+
+     } else if(temperature >= -10 && temperature < 0) {
+          // console.log(`its alright..kinda cold`);
+          console.log(runningAttire[1].clothing);
+
+     } else if(temperature >= 0 && temperature < 5) {
+          // console.log(`i'm temperate`);
+          console.log(runningAttire[2].clothing);
+
+     } else if(temperature >= 5 && temperature < 15) {
+          // console.log(`it's warm out!`);
+          console.log(runningAttire[3].clothing);
+
+     } else if(temperature >= 15 && temperature < 25) {
+          // console.log(`it's getting hot in here!`);
+          console.log(runningAttire[4].clothing);
+     } else {
+          // console.log(`is it wise run outside?`);
+          console.log(runningAttire[5].clothing);
+     }
+}
+
 
 //call init function
 weatherApp.init();
